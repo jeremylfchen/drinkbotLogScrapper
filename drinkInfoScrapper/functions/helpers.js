9e4650674dffdf023ec2849b5e48481ef4c61718
Jeremy Chen <jeremylfchen@gmail.com>
2020-10-23 17:33:27 -0700

    refactor ingredient sorting

--- drinkInfoScrapper/functions/helpers.js
+++ drinkInfoScrapper/functions/helpers.js
@@ -4,7 +4,12 @@ const Papa = require('papaparse');
 
 
 const parseString = (rawData) => {
-  return Papa.parse(rawData).data[0];
+  return;
+}
+
+const formatFlavors = (rawData, modifier) => {
+  const flavorArr = JSON.parse(Papa.parse(rawData).data[0][18]);
+  modifier(flavorArr);
 }
 
 /**
@@ -13,23 +18,44 @@ const parseString = (rawData) => {
  * @param {object} store | stores all flavor data, gets mutated if new flavor is found
  */
 exports.extractIngredient = (rawData, store) => {
-  const flavorArr = JSON.parse(parseString(rawData)[18]);
-  flavorArr.forEach((obj) => {
-    const flavor = obj.sku.slice(8, 20);
-    if (store[flavor] === undefined) {
-      store[flavor] = 1;
-    }
-  });
+  // const flavorArr = JSON.parse(parseString(rawData)[18]);
+  // flavorArr.forEach((obj) => {
+  //   const flavor = obj.sku.slice(8, 20);
+  //   if (store[flavor] === undefined) {
+  //     store[flavor] = 1;
+  //   }
+  // });
+  formatFlavors(rawData, (flavorArr) => {
+    flavorArr.forEach((obj) => {
+      const flavor = obj.sku.slice(8, 20);
+      if (store[flavor] === undefined) {
+        store[flavor] = 1;
+      }
+    });
+  })
 }
 
 
 exports.createIngredientCSV = async (data, fileName) => {
-  const flavors = Object.keys(data).sort((a, b) => a > b).join(',');
+  const flavors = Object.keys(data).sort().join(',');
   try {
     await fs.writeFile(path.resolve(__dirname, `../output/${fileName}.csv`), flavors);
     console.log('writing done!');
   } catch (err) {
     console.error('error found ', err.message);
   }
+}
+
+exports.generateDrinkObj = (rawData, store) => {
+  const parsedStr = parseString(rawData);
+  var sku = parsedStr[0];
+  var metaData = {
+    name: parsedStr[1],
+    ingredients: flavorArr
+  }
+
+}
+
+exports.generateDrinkCSV = (drinkData, ingredientsObj, name) => {
 
 }
--- drinkInfoScrapper/functions/scrapDrinks.js
+++ drinkInfoScrapper/functions/scrapDrinks.js
@@ -2,7 +2,6 @@ const fs = require('fs/promises');
 const lineReader = require('line-reader');
 const path = require('path');
 const Promise = require('bluebird');
-// const headers = 'sku,name,version,description,item_type,super_owner,creator,drink_type,drink_class,photo,country,process_order,blender,unit_cost_per_ml,invoice_price,status,fixed_level,default_volume,ingredient';
 
 /**
  *
@@ -10,7 +9,7 @@ const Promise = require('bluebird');
  * @param {*} processer | function to extract data from csv
  * @param {*} writer | writer function to make new csv
  */
-exports.parseFile = async (name, processor, writer = (data, name) => { }) => {
+exports.parseFile = async (name, processor, writer) => {
   var files = [];
   var data = {};
   try {
@@ -20,6 +19,35 @@ exports.parseFile = async (name, processor, writer = (data, name) => { }) => {
       processor(line, data);
     });
     writer(data, name);
+  } catch (err) {
+    console.error('error found: ', err.message);
+  }
+}
+
+exports.generateCSV = async (name, processor, writer) => {
+  var ingredientsObj = {};
+  var ingredientsArr = [];
+  var drinkData = {};
+  //init sorted ingredients array
+  try {
+    var eachLine = Promise.promisify(lineReader.eachLine);
+    await eachLine(path.resolve(__dirname, '../output/ingredients.csv'), (line) => {
+      ingredientsArr = line.split(',');
+      ingredientsArr.forEach((ingredient, idx) => {
+        ingredientsObj[ingredient] = idx;
+      });
+    });
+  } catch (err) {
+    console.error('error found: ', err.message);
+  }
+  //create csv
+  try {
+    files = await fs.readdir(path.resolve(__dirname, '../data'), 'utf-8');
+    var eachLine = Promise.promisify(lineReader.eachLine);
+    await eachLine(path.resolve(__dirname, `../data/${files[1]}`), (line) => {
+      processor(line, drinkData);
+    });
+    writer(drinkData, ingredientsObj, name);
   } catch (err) {
     console.error('error found: ', err.message);
   }
